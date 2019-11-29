bdd9e8c04e5e5eafe46c455b8af9a4818ebf718e
Shannon <shannonie32@gmail.com>
2019-11-30 03:08:19 +0800

    (2019.11.29)

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        int res;
-            
-    if (stones.size() < 3)
-        (stones.size() == 1) ? res = stones[0] : res = abs(stones[0] - stones[1]);
-    else
-    {
-        do{
-            sort(stones.begin(), stones.end(), greater<int>());
-            
-            if(stones[0]!=stones[1])
-                stones.push_back(stones[0]-stones[1]);
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyRoot = new ListNode(0);
+        ListNode* res = dummyRoot;
+        int sum = 0, add=0;
         
-            stones.erase(stones.begin());
-		    stones.erase(stones.begin());
-            
-	    } while(stones.size() > 1);
+        while (l1!=NULL | l2!=NULL) 
+        {
+            if (l1 == NULL)
+            {
+                sum = (l2->val+add)%10;
+                add = (l2->val + add)/10;
+                
+            }
+            else if (l2 == NULL)
+            {
+                sum = (l1->val+add)%10;
+                add = (l1->val + add)/10;
+            }
+            else
+            {
+                sum = (l1->val + l2->val + add)%10;
+                add = (l1->val + l2->val + add)/10;
+            }
+                
+            res->next = new ListNode(sum);
+            res = res->next;
+
+            if(l1 != NULL) l1 = l1->next;
+            if(l2 != NULL) l2 = l2->next;
+        }
+
+        if(add!=0)
+        {
+            res->next = new ListNode(add);
+            res = res->next;
+        }
         
-        (stones.size() == 0)? res=0:res=stones[0];   
-    }
-       
+        res = dummyRoot->next;
+        delete dummyRoot;
+		
         return res;
     }
 };
