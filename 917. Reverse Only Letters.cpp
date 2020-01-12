18ca94f1d341bfa8fa76e490354ffa0263e4860a
Shannon <shannonie32@gmail.com>
2020-01-12 20:40:41 +0800

    (2020.01.03)

--- 917. Reverse Only Letters.cpp
+++ 917. Reverse Only Letters.cpp
@@ -1,3 +1,4 @@
+Method 1
 class Solution {
 public:
     bool isCharacter(char c)
@@ -5,14 +6,31 @@ public:
         int ascii = c-'A'; 
         if((ascii>=0 && ascii<=25) || (ascii>=32 && ascii<=57))
             return true;
+    void rotate(vector<int>& nums, int k) {
         
         return false;
+        if(nums.size()>1)
+        {
+            int last = nums.size()-(k%nums.size());
+            vector<int> front(nums.begin()+last,nums.end());
+            
+            nums.resize(last);
+            nums.reserve(nums.size()+front.size());
+            nums.insert(nums.begin(), front.begin(), front.end());
+        }
     }
     
     string reverseOnlyLetters(string S) {
         int f = 0, r = S.length()-1;
         
         while(r-f>0)
+};
+
+Method 2
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(nums.size()>1)
         {
             if(isCharacter(S[f]) && isCharacter(S[r]))
             {
@@ -25,6 +43,11 @@ public:
                 if(isCharacter(S[f])) r--;
                 else f++;
             }
+            int n = nums.size();
+            k %= n;
+            
+            nums.insert(nums.begin(), nums.end()-k, nums.end());
+            nums.resize(n);
         }
         
         return S;
